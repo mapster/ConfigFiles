0920334f003dc3ca2b4956ff75ad2388b14efe67
Alexander Hoem Rosbach <alexander@rosbach.no>
2020-03-04 11:04:14 +0100

    Update some stuff for lenovo

--- files/i3.pixelate_lock.sh
+++ files/i3.pixelate_lock.sh
@@ -3,6 +3,6 @@ set -e
 
 rm -f /tmp/screen.png
 scrot /tmp/screen.png
-convert /tmp/screen.png -scale 5% -scale 2000% /tmp/screen.png
+convert /tmp/screen.png -scale 2% -scale 5000% /tmp/screen.png
 [[ -f ~/.i3/lock-icon.png ]] && convert /tmp/screen.png ~/.i3/lock-icon.png -gravity center -composite -matte /tmp/screen.png
 i3lock -i /tmp/screen.png
